8ae6cf6d1e4a7d1f40c140f07fc624b73970cd23
KeremTAN <keremtan.95@hotmail.com>
2023-02-13 20:02:58 +0300

    PyInterpreter is updated

--- src/main/java/Python/PyInterpreter.java
+++ src/main/java/Python/PyInterpreter.java
@@ -5,23 +5,12 @@ import java.io.*;
 public class PyInterpreter {
     public void execPy(String pyFile){
         try {
-            String command = "cmd /c python "+System.getProperty("user.dir")+"\\src\\main\\resources\\"+pyFile;
+            String command = "python3 "+System.getProperty("user.dir")+"/src/main/resources/"+pyFile;;
+            String osName = System.getProperty("os.name").toLowerCase();
+            if(osName.contains("windows"))
+                command = "cmd /c python3 "+System.getProperty("user.dir")+"\\src\\main\\resources\\"+pyFile;
             Process p = Runtime.getRuntime().exec(command);
             p.waitFor();
-            BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            BufferedReader bre = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-            String line;
-            while ((line = bri.readLine()) != null) {
-                System.out.println(line);
-            }
-            bri.close();
-            while ((line = bre.readLine()) != null) {
-                System.out.println(line);
-            }
-            bre.close();
-            p.waitFor();
-            System.out.println("Done.");
-
             p.destroy();
         }
         catch (Exception e){
